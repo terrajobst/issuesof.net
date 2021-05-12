6b2409e3b1729d14f153c45fce06d6fb8bf60956
Immo Landwerth <immo@landwerth.net>
2021-05-11 23:49:48 -0700

    Add ability to query for relative dates
    In TFS one could use `@today`, `@today-x`, and `@today+x` to express relative
    dates. This adds ability to use this syntax where date ranges are permitted,
    e.g.
    
        created:<@today-10
    
    would find all issues that were created more than 10 days ago.

--- src/IssueDb/Querying/Ranges/RangeSyntax.cs
+++ src/IssueDb/Querying/Ranges/RangeSyntax.cs
@@ -11,7 +11,45 @@ namespace IssueDb.Querying.Ranges
 
         public static RangeSyntax<DateTimeOffset> ParseDateTimeOffset(string text)
         {
-            return RangeSyntax<DateTimeOffset>.Parse(text, DateTimeOffset.TryParse);
+            return RangeSyntax<DateTimeOffset>.Parse(text, TryParseDateTime);
+
+            static bool TryParseDateTime(string text, out DateTimeOffset value)
+            {
+                if (text is null)
+                {
+                    value = default;
+                    return false;
+                }
+
+                if (string.Equals(text.Trim(), "@today", StringComparison.OrdinalIgnoreCase))
+                {
+                    value = DateTimeOffset.UtcNow;
+                    return true;
+                }
+
+                var ops = new[] { "-", "+" };
+                foreach (var op in ops)
+                {
+                    var indexOfOp = text.IndexOf(op, StringComparison.OrdinalIgnoreCase);
+                    if (indexOfOp > 0)
+                    {
+                        var leftText = text.Substring(0, indexOfOp).Trim();
+                        var rightText = text.Substring(indexOfOp + op.Length).Trim();
+
+                        if (string.Equals(leftText, "@today", StringComparison.OrdinalIgnoreCase) &&
+                            int.TryParse(rightText, out var days))
+                        {
+                            if (op == "-")
+                                days = -days;
+
+                            value = DateTimeOffset.UtcNow.AddDays(days);
+                            return true;
+                        }
+                    }
+                }
+
+                return DateTimeOffset.TryParse(text, out value);
+            }
         }
     }
 }
