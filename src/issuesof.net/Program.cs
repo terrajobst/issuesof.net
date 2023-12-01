201a2a141dfada468427429b4a4ce93926526783
Immo Landwerth <immol@microsoft.com>
2023-11-30 19:48:20 -0800

    Use simple Program.cs

--- src/issuesof.net/Program.cs
+++ src/issuesof.net/Program.cs
@@ -1,16 +1,46 @@
-﻿namespace IssuesOfDotNet;
+﻿using IssuesOfDotNet.Data;
 
-public class Program
+using Terrajobst.GitHubEvents;
+using Terrajobst.GitHubEvents.AspNetCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddApplicationInsightsTelemetry(builder.Configuration["APPINSIGHTS_INSTRUMENTATIONKEY"]);
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddHostedService(p => p.GetRequiredService<GitHubEventProcessingService>());
+builder.Services.AddSingleton<IndexService>();
+builder.Services.AddSingleton<CompletionService>();
+builder.Services.AddSingleton<GitHubEventProcessor, EventService>();
+builder.Services.AddSingleton<GitHubEventProcessingService>();
+builder.Services.AddSingleton<SearchService>();
+
+var app = builder.Build();
+
+// Warm up key services
+app.Services.GetService<IndexService>();
+app.Services.GetService<CompletionService>();
+app.Services.GetService<GitHubEventProcessingService>();
+
+var gitHubWebHookSecret = app.Configuration["GitHubWebHookSecret"];
+
+if (app.Environment.IsDevelopment())
 {
-    public static void Main(string[] args)
-    {
-        CreateHostBuilder(args).Build().Run();
-    }
-
-    public static IHostBuilder CreateHostBuilder(string[] args) =>
-        Host.CreateDefaultBuilder(args)
-            .ConfigureWebHostDefaults(webBuilder =>
-            {
-                webBuilder.UseStartup<Startup>();
-            });
+    app.UseDeveloperExceptionPage();
 }
+else
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapDefaultControllerRoute();
+app.MapGitHubWebHook(secret: gitHubWebHookSecret);
+app.MapFallbackToPage("/_Host");
+
+app.Run();
