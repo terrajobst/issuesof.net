0e5caf7b9d821a9cd34da95fc1698fe3270c0181
Immo Landwerth <immol@microsoft.com>
2023-12-04 18:38:28 -0800

    Expose notion of being a primary area lead/owner

--- src/IssueDb/Crawling/CrawledAreaMember.cs
+++ src/IssueDb/Crawling/CrawledAreaMember.cs
@@ -11,4 +11,23 @@ public sealed class CrawledAreaMember
 
     public CrawledAreaMemberOrigin Origin { get; }
     public string UserName { get; }
+
+    public bool IsPrimary()
+    {
+        // We consider the origin to be primary if it wasn't expanded
+        // by a team.
+
+        if (Origin is CrawledAreaMemberOrigin.Composite c)
+        {
+            // A composite can also occur if multiple repos define a given member.
+            // in their area-owners.md file.
+            //
+            // In this case we'd still consider it primary.
+            return !c.Origins.Any(c => c is CrawledAreaMemberOrigin.Team);
+        }
+        else
+        {
+            return true;
+        }
+    }
 }
