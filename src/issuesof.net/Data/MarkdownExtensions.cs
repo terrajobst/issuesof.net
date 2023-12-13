5d25ea492f9298b33d2d5ff3858c5cd39d6674eb
Immo Landwerth <immol@microsoft.com>
2023-12-12 22:14:37 -0800

    Expand code in issue titles

--- src/issuesof.net/Data/MarkdownExtensions.cs
+++ src/issuesof.net/Data/MarkdownExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using System.Text;
-
+using System.Text.Encodings.Web;
 using Markdig.Extensions.Emoji;
+using Microsoft.AspNetCore.Components;
 
 namespace IssuesOfDotNet.Data;
 
@@ -8,12 +9,48 @@ public static class MarkdownExtensions
 {
     private static readonly IDictionary<string, string> _emojiMapping = EmojiMapping.GetDefaultEmojiShortcodeToUnicode();
 
-    public static string HighlightCode(this string text)
+    public static MarkupString HighlightCode(this string text)
     {
-        // TODO: We should only expand balanced back ticks to <code></code>
-        //       But we also need to escape HTML.
+        if (!text.Contains('`'))
+            return new MarkupString(HtmlEncoder.Default.Encode(text));
 
-        return text;
+        using var writer = new StringWriter();
+        var p = 0;
+
+        while (p < text.Length)
+        {
+            if (!GetTicks(text, p, out var firstTick, out var secondTick))
+            {
+                HtmlEncoder.Default.Encode(writer, text, p, text.Length - p);
+                break;
+            }
+
+            var length = secondTick - firstTick - 1;
+            if (length > 0)
+            {
+                HtmlEncoder.Default.Encode(writer, text, p, firstTick - p);
+                writer.Write("<code>");
+                HtmlEncoder.Default.Encode(writer, text, firstTick + 1, secondTick - firstTick - 1);
+                writer.Write("</code>");
+            }
+
+            p = secondTick + 1;
+        }
+
+        static bool GetTicks(string text, int start, out int firstTick, out int secondTick)
+        {
+            firstTick = text.IndexOf('`', start);
+            if (firstTick < 0)
+            {
+                secondTick = firstTick;
+                return false;
+            }
+
+            secondTick = text.IndexOf('`', firstTick + 1);
+            return secondTick >= 0;
+        }
+
+        return new MarkupString(writer.ToString());
     }
 
     public static string ExpandEmojis(this string text)
